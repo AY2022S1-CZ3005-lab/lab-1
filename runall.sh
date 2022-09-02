c61e7d4448fe77dbb8775338d41bc10973d777ed
BotBw <wang1570@e.ntu.edu.sg>
2022-09-02 17:54:36 +0800

    aaa

--- runall.sh
+++ runall.sh
@@ -1,4 +1,7 @@
 python3 check.py
-python3 task1.py > task1.out
-python3 task2.py > task2.out
-python3 task3.py > task3.out
+echo task1
+time python3 task1.py > task1.out
+echo task2
+time python3 task2.py > task2.out
+echo task3
+time python3 task3.py > task3.out
