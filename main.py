de937e46072f0801e93a863a60f537f03348209a
BotBw <wang1570@e.ntu.edu.sg>
2022-09-08 16:35:32 +0800

    beautify

---
+++ main.py
@@ -0,0 +1,8 @@
+# scripts
+import check
+import task1
+import task2
+import task3
+
+if __name__ == '__main__':
+  print('\n======== Scripts terminate ========')
--- runall.sh
+++
@@ -1,4 +0,0 @@
-python3 check.py
-python3 task1.py 
-python3 task2.py 
-python3 task3.py
